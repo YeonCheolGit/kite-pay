473ba1a56541cdcb5341c87c044b8b53694b05c7
YeonCheol <yeoncheol.jang@gmail.com>
2022-12-29 01:24:09 +0900

    init

---
+++ barcode/src/main/java/com/kite/barcode/BarcodeApplication.java
@@ -0,0 +1,11 @@
+package com.kite.barcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarcodeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BarcodeApplication.class, args);
+    }
+}
